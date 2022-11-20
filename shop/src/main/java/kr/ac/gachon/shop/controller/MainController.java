49a0e2fde2482d8fcdf26b6b8e1e62dc25010c01
NAMWONSHIK <nws717@naver.com>
2022-11-21 02:29:21 +0900

    mybag Commit

--- shop/src/main/java/kr/ac/gachon/shop/controller/MainController.java
+++ shop/src/main/java/kr/ac/gachon/shop/controller/MainController.java
@@ -1,6 +1,5 @@
 package kr.ac.gachon.shop.controller;
 
-import ch.qos.logback.classic.Logger;
 import groovy.util.logging.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,8 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
     @GetMapping("/")
     public String index(Model model){
-        Logger log = null;
-        log.info("main 컨트롤러");
         return "main";
     }
 }
