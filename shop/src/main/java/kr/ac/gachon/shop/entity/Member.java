67e068c7ad14a9556c727c9b1245eb7c5459d582
NAMWONSHIK <nws717@naver.com>
2022-11-23 21:45:55 +0900

    mybackpack 11.23 16:39 Commit

--- shop/src/main/java/kr/ac/gachon/shop/entity/Member.java
+++ shop/src/main/java/kr/ac/gachon/shop/entity/Member.java
@@ -24,6 +24,7 @@ public class Member //extends BaseEntity
     @Id
     @Column(name="member_id")
     @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
     private String name;
 
